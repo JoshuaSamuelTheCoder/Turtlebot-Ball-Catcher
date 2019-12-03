24080b2068b8e2e1d55ea626b712ce416ccff26f
Class Account <ee106a-agv@c111-6.local>
2019-12-02 16:36:02 -0800

    Fully can work with 2 cameras.

--- working_2_cameras.py
+++ working_2_cameras.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 import threading
 from threading import Thread, Event, ThreadError
-import sys
 
 hue = 10
 sat = 10
@@ -10,31 +9,28 @@ val = 10
 hue2 = 180
 sat2 = 255  
 val2 = 255
-camera_num = int(sys.argv[1])
-camera_string = 'Camera_' + str(camera_num)
-objeto_string = 'Objeto_' + str(camera_num)
 
 class Cam():
   def nothing():
     pass
   def run(self):
+
     points = []
     global hue
     global sat
     global val
-   
-    cap = cv2.VideoCapture(camera_num)
+    cap = cv2.VideoCapture(0)
 
     while True:
       try:
         _,frame = cap.read()
 
-        h = cv2.getTrackbarPos('H',objeto_string)
-        s = cv2.getTrackbarPos('S',objeto_string)
-        v = cv2.getTrackbarPos('V',objeto_string)
-        h2 = cv2.getTrackbarPos('H',camera_string)
-        s2 = cv2.getTrackbarPos('S',camera_string)
-        v2 = cv2.getTrackbarPos('V',camera_string)
+        h = cv2.getTrackbarPos('H','Objeto')
+        s = cv2.getTrackbarPos('S','Objeto')
+        v = cv2.getTrackbarPos('V','Objeto')
+        h2 = cv2.getTrackbarPos('H','Camera')
+        s2 = cv2.getTrackbarPos('S','Camera')
+        v2 = cv2.getTrackbarPos('V','Camera')
 
         lower = np.array([h,s,v])
         upper = np.array([h2,s2,v2])
@@ -60,7 +56,7 @@ class Cam():
         for c in contours:
           x,y,w,h = cv2.boundingRect(c)
           cv2.rectangle(dilation, (x, y), (x+w, y+h), (255, 0, 255), 2)
-        cv2.imshow(objeto_string, dilation)
+        cv2.imshow('Objeto', dilation)
 
         if cv2.waitKey(1) ==1048603:
           exit(0)
@@ -69,14 +65,14 @@ class Cam():
       except ThreadError:
         self.thread_cancelled = True
 
-  cv2.namedWindow(camera_string)
-  cv2.namedWindow(objeto_string)
-  cv2.createTrackbar('H', objeto_string, hue, 180, nothing)
-  cv2.createTrackbar('S', objeto_string, sat, 255, nothing)
-  cv2.createTrackbar('V', objeto_string, val, 255, nothing)
-  cv2.createTrackbar('H', camera_string, hue2, 180, nothing)
-  cv2.createTrackbar('S', camera_string, sat2, 255, nothing)
-  cv2.createTrackbar('V', camera_string, val2, 255, nothing)
+  cv2.namedWindow('Camera')
+  cv2.namedWindow('Objeto')
+  cv2.createTrackbar('H', 'Objeto', hue, 180, nothing)
+  cv2.createTrackbar('S', 'Objeto', sat, 255, nothing)
+  cv2.createTrackbar('V', 'Objeto', val, 255, nothing)
+  cv2.createTrackbar('H', 'Camera', hue2, 180, nothing)
+  cv2.createTrackbar('S', 'Camera', sat2, 255, nothing)
+  cv2.createTrackbar('V', 'Camera', val2, 255, nothing)
         
  
   
